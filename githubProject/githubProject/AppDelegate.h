96ec358c66ac8be4a7ddc26294af6243dfe1c80d
wxw <predecessor@wxws-MacBook-Pro.local>
2018-01-02 10:40:40 +0800

    上传项目

---
+++ githubProject/githubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubProject
+//
+//  Created by wxw on 2018/1/2.
+//  Copyright © 2018年 wxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubProject/githubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubProject
+//
+//  Created by wxw on 2018/1/2.
+//  Copyright © 2018年 wxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
